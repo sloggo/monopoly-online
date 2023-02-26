e041759e448e720517e3ff8ceb9c3997896dafdc
sloggo <70816908+sloggo@users.noreply.github.com>
2023-02-26 16:06:14 +0000

    Add server and basic server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import Board from './components/Board';
+import io from "socket.io-client";
+import { useEffect } from 'react';
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+
+  function sendMessage(){
+    socket.emit("send-message", {message:"data"})
+  }
+
+  useEffect(() => {
+
+    socket.on("receive-message", (data) => {
+      alert(data.message)
+    })
+    
+  }, [socket])
+
   return (
-    <div className="App">
+    <div className="App" onClick={sendMessage}>
       <Board></Board>
     </div>
   );
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected: "+socket.id)
+
+    socket.on("send-message", (data) => {
+        socket.broadcast.emit("receive-message", data)
+    })
+})
+
+server.listen(3001, () => {
+    console.log("Server running!")
+})

a93e006e917c0c84a88ee6bbcd2147700b526a2c
sloggo <70816908+sloggo@users.noreply.github.com>
2023-03-04 15:57:10 +0000

    Add options tab

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Board from './components/Board';
 import io from "socket.io-client";
 import { useEffect, useState } from 'react';
 import Home from "./components/Home"
+import Options from './components/Options'
 
 function App() {
   const [currentTab, setCurrentTab] = useState("home")
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Home visible={currentTab === "home"} setOptionsTab={setOptionsTab}></Home>
+      <Options visible={currentTab === "options"} currentTab={currentTab}></Options>
     </div>
   );
 }
---
+++ client/src/components/Options.js
@@ -0,0 +1,30 @@
+import './Options.scss'
+
+import React, { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion"
+
+export default function Options(props) {
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0})
+    const [visible, setVisible] = useState(props.visible)
+
+    const changeMousePos = (ev) => {
+        setMousePosition({x: ev.pageX, y: ev.pageY})
+    }
+
+    useEffect(() =>{
+        setVisible(props.visible)
+      }, [props.currentTab])
+
+
+  return (
+    <AnimatePresence>
+      { visible && <motion.div className='home-container' onMouseMove={(ev) => changeMousePos(ev)} transition={{duration:1}} initial={{ y:-3000 }} animate={{ y:-0 }} exit={{ y:2000 }}>
+        <div className='ball-blur'>
+            <motion.div className='ball-of-colour' animate={{x: mousePosition.x - 200, y: mousePosition.y - 200}} transition={{ duration: .05, type: "tween" }}/>
+        </div>
+
+        <h2 className='options-header'>Let's get straight into a game!</h2>
+      </motion.div>}
+    </AnimatePresence>
+  )
+}

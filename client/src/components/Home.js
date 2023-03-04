8c5aa3fb20f0f1c47ce0e928a331d7a95910d46f
sloggo <70816908+sloggo@users.noreply.github.com>
2023-03-04 15:33:54 +0000

    Add header buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,12 +12,18 @@ export default function Home() {
   return (
     <div className='home-container' onMouseMove={(ev) => changeMousePos(ev)}>
         <div className='ball-blur'>
-            <motion.div className='ball-of-colour' animate={{x: mousePosition.x - 250, y: mousePosition.y - 250}} transition={{ duration: .05, type: "tween"  }}/>
+            <motion.div className='ball-of-colour' animate={{x: mousePosition.x - 200, y: mousePosition.y - 200}} transition={{ duration: .05, type: "tween"  }}/>
         </div>
 
-        <div className='text-holder'>
-          <h1 className='home-header'>monopoly</h1>
-          <motion.h1 className='home-header' initial={{x: 0, y: 100}} animate={{x:0, y:0}} transition={{delay:.3, duration:1}}>online</motion.h1>
+        <div className='home-header'>
+          <p>Home</p>
+          
+          <div className='text-holder'>
+            <h1 className='home-header'>monopoly</h1>
+            <motion.h1 className='home-header' initial={{x: 0, y: 100}} animate={{x:0, y:0}} transition={{delay:.3, duration:1}}>online</motion.h1>
+          </div>
+          
+          <p>Account</p>
         </div>
 
         <div className='start-playing'>Start Playing</div>

7358cd008699a0fcb36a28a2bd7faa3b234a4577
sloggo <70816908+sloggo@users.noreply.github.com>
2023-03-05 20:16:04 +0000

    Add ready up button

--- client/src/components/WaitingRoom.js
+++ client/src/components/WaitingRoom.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from "framer-motion"
+import './WaitingRoom.scss'
 
 export default function WaitingRoom(props) {
     const [mousePosition, setMousePosition] = useState({x: 0, y: 0})
     const [visible, setVisible] = useState(props.visible)
     const [boardData, setBoardData] = useState(null);
     const [playerData, setPlayerData] = useState(props.playerData)
+    const [ready, setReady] = useState(false)
 
     const changeMousePos = (ev) => {
         setMousePosition({x: ev.pageX, y: ev.pageY})
+    }
+
+    const clickReady = () => {
+        setReady(!ready)
     }
 
     useEffect(()=>{
@@ -27,7 +33,7 @@ export default function WaitingRoom(props) {
         {boardData && <h2 className='options-header'>Lobby: {boardData._id}</h2>}
 
         <div className='players-container'>
-            {boardData && boardData.players.map(playerArray => playerArray._id === playerData._id ? <div className='player'>{playerArray.username} - You</div> : <div className='player'>{playerArray.username}</div>)}
+            {boardData && boardData.players.map(playerArray => playerArray._id === playerData._id ? <div className='player you'><p>{playerArray.username} - You</p> <div className={ready ? "readyup ready": "readyup"} onClick={clickReady}>Ready Up</div> </div> : <div className='player'>{playerArray.username}</div>)}
         </div>
       </motion.div>}
     </AnimatePresence>

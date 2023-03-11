19ab15908a99a7a1c43a82d84834b8267b615994
sloggo <70816908+sloggo@users.noreply.github.com>
2023-03-11 11:02:14 +0000

    Add grid system
    Using an offset from the side of the map, align player to a grid that can be used with the goto function to move player around the map

--- client/src/components/BoardAlt.js
+++ client/src/components/BoardAlt.js
@@ -1,17 +1,48 @@
 import React, {useEffect, useRef, useState} from 'react'
 import mapPng from '../assets/map.png'
 import playerPng from '../assets/TX Player.png'
+import boardDatas from './boardData.json'
 let c;
 
 export default function BoardAlt() {
     const canvasRef = useRef(null)
     const [background, setBackground] = useState({
         image: mapPng,
+        offset:{
+            x:-576,
+            y:-420
+        },
         position: {
-            x: -890,
-            y: -540
-        }
+            x: 30,
+            y: 20
+        },
+        tileSize: 32*2 //1.5 zoom
     })
+
+    const [grid, setGrid] = useState([
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+    ])
+    const [boardData, setBoardData] = useState(boardDatas)
 
     const [movementCounter, setMovementCounter] = useState(0)
 
@@ -33,7 +64,7 @@ export default function BoardAlt() {
         const playerImage = new Image()
         playerImage.src = playerSprite.image
 
-        c.drawImage(image, background.position.x, background.position.y)
+        c.drawImage(image, (-(background.position.x)*background.tileSize)-background.offset.x, (-(background.position.y)*background.tileSize-background.offset.y))
         c.drawImage(
                 playerImage,
                 0,
@@ -53,28 +84,28 @@ export default function BoardAlt() {
         if(e.key === 'w'){
             console.log('w')
             let newBackground = {...background}
-            newBackground.position.y += 10
+            newBackground.position.y -= 1
             setBackground(newBackground)
         }
 
         if(e.key === 's'){
             console.log('s')
             let newBackground = {...background}
-            newBackground.position.y -= 10
+            newBackground.position.y += 1
             setBackground(newBackground)
         }
 
         if(e.key === 'a'){
             console.log('a')
             let newBackground = {...background}
-            newBackground.position.x += 10
+            newBackground.position.x -= 1
             setBackground(newBackground)
         }
 
         if(e.key === 'd'){
             console.log('d')
             let newBackground = {...background}
-            newBackground.position.x -= 10
+            newBackground.position.x += 1
             setBackground(newBackground)
         }
     }
@@ -84,28 +115,37 @@ export default function BoardAlt() {
         window.addEventListener("keydown", handleUserKey)
         window.requestAnimationFrame(render)
 
-        goTo(0, 0)
     }, [])
 
     function goTo(x, y){
         setTimeout(() => {
-            let newMovementCounter = movementCounter
-            setMovementCounter(newMovementCounter+1)
             console.log("goto", x, y)
 
-            if(background.position.x !== x){
+            if(background.position.x !== x && background.position.x > x){
                 let newBack = {...background}
 
-                newBack.position.x += 10
+                newBack.position.x -= .5
                 setBackground(newBack)
                 goTo(x,y)
-            } else if(background.position.y !== y){
+            } else if(background.position.x !== x && background.position.x < x){
                 let newBack = {...background}
 
-                newBack.position.y += 10
+                newBack.position.x += .5
                 setBackground(newBack)
                 goTo(x,y)
-            } 
+            } else if(background.position.y !== y && background.position.y > y){
+                let newBack = {...background}
+
+                newBack.position.y -= .5
+                setBackground(newBack)
+                goTo(x,y)
+            } else if(background.position.y !== y && background.position.y < y){
+                let newBack = {...background}
+
+                newBack.position.y += .5
+                setBackground(newBack)
+                goTo(x,y)
+            }
         }, 100)
     }
 

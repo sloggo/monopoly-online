18b6c95fc434d0bf802729bf154e8fb5d5bbd216
sloggo <70816908+sloggo@users.noreply.github.com>
2023-03-12 21:30:44 +0000

    Begin working on the proper city map

---
+++ tiled/Modern_Exteriors_Complete_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="36960" columns="176">
+ <image source="../../../../Downloads/modernexteriors-win/Modern_Exteriors_16x16/Modern_Exteriors_Complete_Tileset.png" width="2816" height="3360"/>
+</tileset>
